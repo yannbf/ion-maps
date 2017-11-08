7e40eee707864122db4c9b86deaa569866b2f98a
Yann Braga <yannbf@gmail.com>
2017-11-07 22:21:30 -0300

    feat(maps-config): add google maps loader + config file

---
+++ src/providers/javascript-google-maps/google-maps.config.ts
@@ -0,0 +1,5 @@
+export const mapsConfig = {
+  version: '3.30',
+  url: 'https://maps.google.com/maps/api/',
+  apiKey: 'AIzaSyBaTVlHDndpSgbdDnRsCy2xFJt2tB41NB0',
+}
---
+++ src/providers/javascript-google-maps/google-maps.loader.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { mapsConfig } from './google-maps.config';
+
+@Injectable()
+export class GoogleMapsLoader {
+    private static promise: Promise<any>;
+
+    public static load(): Promise<any> {
+        // First time 'load' is called?
+        if (!GoogleMapsLoader.promise) {
+            // Make promise to load
+            GoogleMapsLoader.promise = new Promise( resolve => {
+                // Set callback for when google maps is loaded.
+                window['__onGoogleMapsLoaded'] = (ev) => {
+                    resolve('google maps api loaded');
+                };
+
+                const { url, version, apiKey } = mapsConfig;
+                const mapsNode = document.createElement('script');
+                mapsNode.src = `${url}js?v=${version}&key=${apiKey}&callback=__onGoogleMapsLoaded`;
+                mapsNode.type = 'text/javascript';
+                document.getElementsByTagName('head')[0].appendChild(mapsNode);
+            });
+        }
+
+        // Always return promise. When 'load' is called many times, the promise is already resolved.
+        return GoogleMapsLoader.promise;
+    }
+}
