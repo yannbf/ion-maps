235209e0c5d0dd712de22daf9e472eff93545219
Yann Braga <yannbf@gmail.com>
2017-11-09 01:18:03 -0300

    feat(static-maps): add options for customization

--- src/components/ion-static-maps/ion-static-maps.ts
+++ src/components/ion-static-maps/ion-static-maps.ts
@@ -1,6 +1,6 @@
 import { mapStyles } from '../../providers/maps/maps.styles';
 import { mapSettings } from '../../providers/maps/javascript-google-maps/google-maps.settings';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ion-static-maps',
@@ -8,24 +8,43 @@ import { Component } from '@angular/core';
 })
 export class IonStaticMapsComponent {
 
+  @Input() address: string;
+  @Input() lat: number;
+  @Input() lng: number;
+  @Input() options: StaticIonMapOptions;
+
   mapsUrl: string;
 
-  constructor() {
+  ngAfterViewInit() {
+    console.log(this.address, this.lat, this.lng)
     this.mapsUrl = this.generateMapsUrl();
   }
 
   generateMapsUrl() {
-    let address = 'New York';
-    let latLng = '40.714728,-73.998672';
-    let mapType = 'roadmap';
-    let zoom = 16;
+    let latLng: string;
+    if(this.lat && this.lng) {
+      latLng = `${this.lat},${this.lng}`;
+    }
+
+    let {
+      zoom,
+      width,
+      height,
+      mapType,
+      language,
+      format,
+      style
+    } = this.options;
+
     const url = mapSettings.staticMapsUrl
       + 'key=' + mapSettings.apiKey
-      + '&center=' + (latLng ? latLng : address)
-      + '&zoom=' + zoom
-      + '&size=600x300'
-      + '&maptype=' + mapType
-      + this.convertMapStyles(mapStyles.military)
+      + '&center=' + (latLng ? latLng : this.address)
+      + '&zoom=' + (zoom || 15)
+      + '&size=' + (width || 700) + 'x' + (height|| 700)
+      + ( mapType ? ('&maptype=' + mapType) : '' )
+      + ( language ? ('&language=' + language) : '' )
+      + ( format ? ('&format=' + format) : '' )
+      + ( style ? (this.convertMapStyles(style)) : '' );
 
     return url;
   }
@@ -54,6 +73,30 @@ export class IonStaticMapsComponent {
     // static maps url only takes hex color in the given format: 0xRRGGBB, so we have to convert
     styleStr = styleStr.replace(/#/g,'0x');
 
+    // encoding is needed to build a valid url: https://developers.google.com/maps/web-services/overview#BuildingURLs
     return encodeURI(styleStr);
   }
+}
+
+export interface StaticIonMapOptions {
+  zoom?: number,
+  width?: number,
+  height?: number,
+  mapType?: StaticIonMapType,
+  style?: Array<any>,
+  format?: StaticIonMapFormat,
+  language?: string,
+}
+
+export enum StaticIonMapType {
+  roadmap = 'roadmap',
+  satellite = 'satellite',
+  hybrid = 'hybrid',
+  terrain = 'terrain'
+}
+
+export enum StaticIonMapFormat {
+  png = 'png',
+  jpeg = 'jpeg',
+  gif = 'gif'
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,5 @@
+import { mapStyles } from '../../providers/maps/maps.styles';
+import { StaticIonMapFormat, StaticIonMapOptions, StaticIonMapType } from '../../components/ion-static-maps/ion-static-maps';
 import { IonMaps } from '../../components/ion-maps/ion-maps';
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
@@ -9,6 +11,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   @ViewChild(IonMaps) ionMaps: IonMaps;
+  mapOptions: StaticIonMapOptions = {};
 
   constructor(
     public navCtrl: NavController,
