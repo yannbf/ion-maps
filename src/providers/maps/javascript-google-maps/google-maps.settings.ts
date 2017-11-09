fdb4d9dc6c35d6a6ea9ead1d9a1b4f3c202f5016
Yann Braga <yannbf@gmail.com>
2017-11-09 00:26:06 -0300

    chore(settings): add static maps url

--- src/providers/maps/javascript-google-maps/google-maps.settings.ts
+++ src/providers/maps/javascript-google-maps/google-maps.settings.ts
@@ -2,4 +2,5 @@ export const mapSettings = {
   version: '3.30',
   url: 'https://maps.google.com/maps/api/',
   apiKey: 'AIzaSyBaTVlHDndpSgbdDnRsCy2xFJt2tB41NB0',
+  staticMapsUrl: 'https://maps.googleapis.com/maps/api/staticmap?'
 }
