a15e335845cbdadaa32de5637b2252ceb53e0794
Yann Braga <yannbf@gmail.com>
2017-11-27 23:15:17 -0300

    refactor: change module structure

--- src/ion-maps.module.ts
+++ src/ion-maps.module.ts
@@ -1,28 +1,47 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { IonMaps } from './components/ion-maps';
-import { IonStaticMapsComponent } from './components/ion-static-maps';
-import { IonMarker } from './components/ion-marker';
-import { IonMapsProvidersModule } from './providers/providers.module';
+import { GoogleMaps } from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Platform } from 'ionic-angular';
 
+import {
+  IonMaps,
+  IonStaticMapsComponent,
+  IonMarker,
+  NativeGoogleMapsProvider,
+  JavascriptGoogleMapsProvider,
+  } from './index';
+
+// Need factories for every provider that
+// changes based on app runtime context
+export function gmapsProviderFactory(platform: Platform, geolocation: Geolocation, gmaps: GoogleMaps) {
+  return this.platform.ready()
+    .then(_ => {
+        return GoogleMaps.installed()
+          ? new NativeGoogleMapsProvider(geolocation, gmaps)
+          : new JavascriptGoogleMapsProvider(geolocation);
+    });
+}
+
+export const COMPONENTS = [
+  IonMaps,
+  IonStaticMapsComponent,
+  IonMarker,
+]
 @NgModule({
-  declarations: [
-    IonMaps,
-    IonStaticMapsComponent,
-    IonMarker,
-  ],
-	imports: [
-    IonMapsProvidersModule.forRoot(),
-  ],
-	exports: [
-    IonMaps,
-    IonStaticMapsComponent,
-    IonMarker,
-  ]
+  declarations: [...COMPONENTS],
+	exports: [...COMPONENTS]
 })
 export class IonMapsModule {
   static forRoot(config: any): ModuleWithProviders {
     return {
-      ngModule: IonMapsModule
+      ngModule: IonMapsModule,
+      providers: [
+        {
+          provide: NativeGoogleMapsProvider,
+          useFactory: gmapsProviderFactory,
+          deps: [Geolocation, GoogleMaps],
+        },
+      ],
     };
   }
 }
